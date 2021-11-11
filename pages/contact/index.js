5b1dc34d5b2464338e39c3d85caf86b9795c3fee
hongchan88 <33241580+hongchan88@users.noreply.github.com>
2021-11-11 11:51:25 +1100

    edit

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -17,9 +17,7 @@ export default function Contact() {
                 <h1 className="text-gray-50 font-semibold text-2xl">
                   Get in touch, let's talk.
                 </h1>
-                <p className="font-light text-base text-gray-200 mt-2">
-                  Fill in the details and I'll get back to you as soon as I can.
-                </p>
+                <p className="font-light text-base text-gray-200 mt-2"></p>
               </header>
               <div className="icons-container inline-flex flex-col my-20">
                 <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
