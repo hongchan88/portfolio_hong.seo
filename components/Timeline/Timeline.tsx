744a5bb16707d098fb317baec705c919a1a013f2
Hong <hongchan88@hotmail.com>
2025-04-22 22:14:12 +1000

    update text for portfolio

--- components/Timeline/Timeline.tsx
+++ components/Timeline/Timeline.tsx
@@ -69,8 +69,8 @@ export const Timeline = () => {
       ref={containerRef}
     >
       <div className='max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10'>
-        <h2 className='text-lg font-bold font-mono md:text-6xl mb-4 text-black dark:text-white max-w-4xl'>
-          Things I've worked on
+        <h2 className='text-lg font-bold font-mono md:text-4xl mb-4 text-black dark:text-white max-w-4xl'>
+          Some things I’ve worked on
         </h2>
       </div>
 
