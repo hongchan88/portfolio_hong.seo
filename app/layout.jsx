3c95c8f45a52710efe865ea07c5084f4c9a8a29b
Hong <hongchan88@hotmail.com>
2025-04-28 11:15:13 +1000

    change path

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Footer from '../components/Footer';
 import { ThemeProvider } from 'next-themes';
 import { Space_Mono, Rubik } from 'next/font/google';
 import dynamic from 'next/dynamic';
-const ScrollRestoration = dynamic(() => import('@utils/ScrollRestoration'), {
+const ScrollRestoration = dynamic(() => import('./utils/ScrollRestoration'), {
   ssr: false,
 });
 const spaceMono = Space_Mono({
