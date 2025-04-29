59c239ade66df0496e36db0ce293cdf5cc43dbdb
Hong <hongchan88@hotmail.com>
2025-04-29 13:10:04 +1000

    hydratingLoading to have animation

--- components/HydrationLoading/index.tsx
+++ components/HydrationLoading/index.tsx
@@ -1,15 +1,14 @@
 'use client';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 
 export default function HydrationLoading() {
   return (
-    <div className='fixed inset-0 h-full w-full bg-greenYellowGradient z-[9999] flex flex-col items-center justify-center  pointer-events-none'>
-      <div className='relative w-56 h-56 mb-4 overflow-hidden '>
+    <div className='fixed inset-0 h-full w-full bg-greenYellowGradient z-[9999] flex flex-col items-center justify-center pointer-events-none'>
+      <div className='relative w-56 h-56 mb-4 overflow-hidden animate-pulse'>
         <Image
           width={224}
           height={224}
-          src='/loading/loading2.webp' // Replace with your PNG
+          src='/loading/loading2.webp'
           alt='avatar'
           className='absolute top-0 left-0 w-full h-full object-cover opacity-20'
           priority
